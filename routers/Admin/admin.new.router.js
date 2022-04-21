241a9c02abb297015fc2c1f6f42dd598d2dbb8f1
Nguyen Huu Tri <tri.nguyen@dgvdigital.com>
2022-04-21 16:34:27 +0700

    authorize admin

--- routers/Admin/admin.new.router.js
+++ routers/Admin/admin.new.router.js
@@ -1,10 +1,13 @@
 var express = require("express")
 var router = express.Router()
-var controller= require('../../controllers/Admin/admin.new.controller')
+var controller = require('../../controllers/Admin/admin.new.controller')
+var authorize = require('../../midleware/verify.token');
+var role = require('../../common/role');
 
-router.get("/tin-tuc", controller.GetAllNews);
-router.get("/cho-gui-duyet-tin-tuc", controller.GetPendingNews)
-router.post("/chap-nhan-tin-tuc", controller.AcceptNews)
-router.post("/tu-choi-tin-tuc", controller.DenyNews)
 
-module.exports=router;
+router.get("/tin-tuc", authorize(role.ADMIN), controller.GetAllNews);
+router.get("/cho-gui-duyet-tin-tuc", authorize(role.ADMIN), controller.GetPendingNews)
+router.post("/chap-nhan-tin-tuc", authorize(role.ADMIN), controller.AcceptNews)
+router.post("/tu-choi-tin-tuc", authorize(role.ADMIN), controller.DenyNews)
+
+module.exports = router;
