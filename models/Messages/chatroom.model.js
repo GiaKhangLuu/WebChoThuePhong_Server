876d6716d6924312fbd98cde6d0853a41883472c
GiaKhangLuu <waoknight@gmail.com>
2022-05-06 07:36:43 +0700

    load old conversation and message

--- models/Messages/chatroom.model.js
+++ models/Messages/chatroom.model.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
+var Message = require('./message.model')
 var today = new Date();
 var chatRoomSchema = new mongoose.Schema({
-    members: Array,
+    members: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }],
     creatDate: Date
 })
 
@@ -9,11 +13,63 @@ var ChatRoom = mongoose.model('ChatRoom', chatRoomSchema, 'ChatRoom')
 
 module.exports.FindAllRoomsOfUser = async userId => {
     try {
-        const roomIds = await ChatRoom.find({ "members": mongoose.Types.ObjectId(userId) }) ;
-        return roomIds;
+        const rooms = await ChatRoom.
+            find({ 'members': mongoose.Types.ObjectId(userId) })
+        /*
+        const rooms = await ChatRoom.
+            find({ 'members': mongoose.Types.ObjectId(userId) }).
+            populate({ 
+                path: 'members', 
+                match: { _id: { $ne: mongoose.Types.ObjectId(userId) } },
+                select: '_id infor'
+            })
+        */
+        return rooms;
     } catch(err) {
         console.log(err);
         return null;
+    }
+}
+
+module.exports.FindUserInforOfAllRooms = async userId => {
+    try {
+        // Get all rooms of user
+        var rooms = await this.FindAllRoomsOfUser(userId)
+
+
+        // Put all roomId in array to filter
+        const arrExp = [];
+        for(const room of rooms) {
+            const _id = room._id.toString();
+            const expression =  { id_room: mongoose.Types.ObjectId(_id) } ;
+            arrExp.push(expression);
+        }
+
+        rooms =  await Message.GetRoomIdSortByLastMessageTime(arrExp) 
+
+        var rs = []
+
+        for(var room of rooms) {
+            var room_id = room._id
+            var lastMessageTime = room.lastMessageTime
+
+            var room_info = await ChatRoom.
+                findById(room_id).
+                populate({ 
+                    path: 'members', 
+                    match: { _id: { $ne: mongoose.Types.ObjectId(userId) } },
+                    select: '_id infor'
+                })
+            rs.push({ 'room_info': room_info, 
+                      'last_message_time': lastMessageTime })
+
+        } 
+
+        return rs
+
+    } catch(err) {
+        console.log(err)
+        return null
     }
 }
 
--- models/Messages/message.model.js
+++ models/Messages/message.model.js
@@ -6,19 +6,67 @@ var MessageSchema = new mongoose.Schema(
             type:String,
             required:true
         },
+
         message_content: {
-          type: String,
-          require: true
-       },
-      status: {
-        type: int,
-        require: true,
-        default: 0
-      }
+            type: String,
+            require: true
+        },
+        status: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        time: {
+            type: Date,
+            require: true,
+            default: new Date()
+        },
+        id_room: {
+            type: mongoose.Types.ObjectId,
+            require: true
+        }
     },{
         versionKey:false
     }
 );
 
 var Message = mongoose.model('Message', MessageSchema, 'Message')
-module.exports = Message;
+
+module.exports.CreateMessage = async (id_sender, message_content, id_room) => {
+    var message = new Message()
+    message.id_sender = id_sender
+    message.message_content = message_content
+    message.id_room = mongoose.Types.ObjectId(id_room)
+    message = await Message.create(message)
+    return message
+}
+
+module.exports.LoadOldMessages = async (room_id) => {
+    const messages = await Message.aggregate([
+        // Stage 1 - get all records in room by group roomId
+        { $match: { id_room: mongoose.Types.ObjectId(room_id) } },
+        // Stage 2 - sort by time
+        { $sort: { time: 1 } }
+    ]);
+    return messages
+}
+
+module.exports.GetRoomIdSortByLastMessageTime = async (room_ids) => {
+    const conversations = await Message.aggregate([
+        // Stage 1 - get all roomDetails by roomId
+        { $match: { $or: room_ids } },
+        // Stage 2 - find last message time
+        {
+            $group: {
+                _id: "$id_room",
+                lastMessageTime: { $max: "$time" },
+            }
+        },
+        // Stage 3 - sort lastMessageTime descending
+        {
+            $sort: { lastMessageTime: -1 }
+        }
+    ] )
+    return conversations
+}
+//module.exports = Message;
