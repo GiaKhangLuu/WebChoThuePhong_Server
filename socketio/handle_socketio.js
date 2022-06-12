4d0f9ce229de1d7d55c3cb4b20b3e8af0f4ae589
GiaKhangLuu <waoknight@gmail.com>
2022-06-12 13:49:48 +0700

    check plaint message

--- socketio/handle_socketio.js
+++ socketio/handle_socketio.js
@@ -25,39 +25,43 @@ module.exports.ReceiveMessage = async (io, data) => {
     var sender_id = data.IdSender
     var message = data.message
 
-    var rooms = await ChatRoom.FindChatRoom(sender_id, receiver_id)
-    var room_id
-
-    // Sender and receiver havent chatted => create new room 
-    if (rooms.length == 0) {
-        var room = await ChatRoom.CreateRoom([receiver_id, sender_id])
-        room_id = room._id
-        // Add user to chat room
-        await AddUsersToNewRoom(io, room_id, receiver_id, sender_id)
-
-        const sockets = await io.in(`room: ${room_id}`).fetchSockets();
-
+    if (message.images.length == 0 && message.content == '') {
+        return 
     } else {
-        room_id = rooms[0]._id
+        var rooms = await ChatRoom.FindChatRoom(sender_id, receiver_id)
+        var room_id
+
+        // Sender and receiver havent chatted => create new room 
+        if (rooms.length == 0) {
+            var room = await ChatRoom.CreateRoom([receiver_id, sender_id])
+            room_id = room._id
+            // Add user to chat room
+            await AddUsersToNewRoom(io, room_id, receiver_id, sender_id)
+
+            const sockets = await io.in(`room: ${room_id}`).fetchSockets();
+
+        } else {
+            room_id = rooms[0]._id
+        }
+
+        // Handle image message 
+        var image_urls = await HandleMessageImage(message.images)
+        message.images = image_urls
+
+        // Create message
+        Message.CreateMessage(sender_id, message, room_id)
+        io.to(`room: ${room_id}`).emit('renderMessage', {
+            message: message,
+            id_receiver: receiver_id,
+            id_sender: sender_id
+        })
+
+        // Send notification
+        var unread_message_count = await ChatRoom.UnreadMessageCount(receiver_id)
+        io.to(`room: ${room_id}`).emit('notifyMessage', {
+           unread_message_count: unread_message_count
+        })
     }
-
-    // Handle image message 
-    var image_urls = await HandleMessageImage(message.images)
-    message.images = image_urls
-
-    // Create message
-    Message.CreateMessage(sender_id, message, room_id)
-    io.to(`room: ${room_id}`).emit('renderMessage', {
-        message: message,
-        id_receiver: receiver_id,
-        id_sender: sender_id
-    })
-
-    // Send notification
-    var unread_message_count = await ChatRoom.UnreadMessageCount(receiver_id)
-    io.to(`room: ${room_id}`).emit('notifyMessage', {
-        unread_message_count: unread_message_count
-    })
 }
 
 // Set socket's name by userId
